d2e638f337ad369f5fd74fd87ec97de8aa042587
Paul Richards <paul.richards@gmail.com>
2013-01-05 20:53:54 +0000

    Puzzle 9.

---
+++ src/com/pauldoo/euler/puzzle/Puzzle9.scala
@@ -0,0 +1,12 @@
+package com.pauldoo.euler.puzzle
+
+object Puzzle9 extends Puzzle {
+
+  def answer() = {
+    (for (
+      a <- 1 to 1000 / 3;
+      b <- (a + 1) to (1000 - a - 1) / 2;
+      val c = 1000 - a - b if a * a + b * b == c * c
+    ) yield a * b * c).first
+  }
+}
---
+++ test/com/pauldoo/euler/puzzle/Puzzle9Test.scala
@@ -0,0 +1,9 @@
+package com.pauldoo.euler.puzzle
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Puzzle9Test extends PuzzleTest {
+  def puzzle = Puzzle9;
+  def expectedAnswer = 31875000;
+}
