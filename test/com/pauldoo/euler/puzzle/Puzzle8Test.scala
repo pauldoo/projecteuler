cb28f3a31c9d6feb2ad6c65d46a063c15e3f816f
Paul Richards <paul.richards@gmail.com>
2013-01-05 19:52:38 +0000

    Puzzle 8.

---
+++ test/com/pauldoo/euler/puzzle/Puzzle8Test.scala
@@ -0,0 +1,9 @@
+package com.pauldoo.euler.puzzle
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Puzzle8Test extends PuzzleTest {
+  def puzzle = Puzzle8;
+  def expectedAnswer = 40824;
+}
