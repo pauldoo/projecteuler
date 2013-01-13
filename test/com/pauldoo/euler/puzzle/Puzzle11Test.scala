972119f2a94d819067866610d8ca78ddc863a295
Paul Richards <paul.richards@gmail.com>
2013-01-13 21:57:06 +0000

    Puzzle 11.

---
+++ test/com/pauldoo/euler/puzzle/Puzzle11Test.scala
@@ -0,0 +1,9 @@
+package com.pauldoo.euler.puzzle
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Puzzle11Test extends PuzzleTest {
+  def puzzle = Puzzle11;
+  def expectedAnswer = BigInt("70600674");
+}
